7f92080140bf6792d27ff82b2396af1cf1e367f5
Igor <igor.alexandro@gmail.com>
2009-10-27 10:11:36 +0300

    New look of index page

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,17 +1,6 @@
 class Admin::AdminController < ApplicationController
   filter_parameter_logging :password, :password_confirmation
-  helper_method :current_user_session, :current_user
-
-  private
-  def current_user_session
-    return @current_user_session if defined?(@current_user_session)
-    @current_user_session = UserSession.find
-  end
-
-  def current_user
-    return @current_user if defined?(@current_user)
-    @current_user = current_user_session && current_user_session.user
-  end
+  
 
   def require_user
     unless current_user
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  helper_method :current_user_session, :current_user
+
+  private
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
