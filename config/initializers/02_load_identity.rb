4b1b518b1f0d75e8fb2c51635df3f07021290a06
Igor Alexandrov <igor.alexandrov@gmail.com>
2010-02-15 11:42:01 +0300

    Loading of Identity is more effective now

--- config/initializers/02_load_identity.rb
+++ config/initializers/02_load_identity.rb
@@ -6,8 +6,3 @@ unless configatron.general.identity.nil?
   ActiveIdentity.identity_name= configatron.general.identity
 end
 
-module ActionController
-  class Base
-    self.prepend_view_path("#{RAILS_ROOT}/app/identities/#{configatron.general.identity}/views")
-  end
-end
--- lib/r_blog/customization/active_identity.rb
+++ lib/r_blog/customization/active_identity.rb
@@ -1,18 +1,23 @@
 module RBlog
   module Customization
     #Class holds currently active Identity of RBlog.
-    #
-    #@example How to use in controllers.
-    #  class SomeController < ApplicationController
-    #     include RBlog::Customization
-    #     layout ActiveIdentity.layout("some_layout")
-    #  end
-    #
+    #    
     #@example How to use in layouts.
     #  <%= stylesheet_link_tag ActiveIdentity.stylesheet('master') %>
     class ActiveIdentity      
       @@identity_name = nil
       cattr_accessor :identity_name
+      
+      def self.identity_name= (identity_name)
+        @@identity_name = identity_name
+        ActionController::Base.class_eval do
+          #Delete all paths, that are not default
+          self.view_paths.size > 1 ? self.view_paths.delete_at(-1) : true
+          #Prepend view paths with path for current Identity
+          self.prepend_view_path("#{RAILS_ROOT}/app/identities/#{@@identity_name}/views")
+        end
+      end
+
       #Loads full Identity as object.
       #Use it only if you want to do something with Identity, for example to show it's parameters.
       #
@@ -26,21 +31,21 @@ module RBlog
       #
       #@param [String] name name of requested layout;
       #@return [String] path to requestred layout;
-      def self.layout(name)
-        if has?(:layout, name)
-          File.join(Identity.identities_root, self.identity_name, "/layouts/", name + ".html.erb")
-        else
-          name
-        end
-      end
-
-      def self.view(name)
-        if has?(:view, name)
-          File.join(Identity.identities_root, self.identity_name, "/views/", name + ".html.erb")
-        else
-          name
-        end
-      end
+#      def self.layout(name)
+#        if has?(:layout, name)
+#          File.join(Identity.identities_root, self.identity_name, "/layouts/", name + ".html.erb")
+#        else
+#          name
+#        end
+#      end
+#
+#      def self.view(name)
+#        if has?(:view, name)
+#          File.join(Identity.identities_root, self.identity_name, "/views/", name + ".html.erb")
+#        else
+#          name
+#        end
+#      end
 
       #Get the path of stylesheet file for current Identity by name.
       #If current Identity doesn't have such file, get path of default file.
@@ -55,18 +60,18 @@ module RBlog
         self.identity_name.nil? ? name : File.join("/identities", self.identity_name, "images", name )
       end
 
-      private
-      def self.has?(type, object_name)
-        case type
-        when :layout
-          File.file? File.join(Identity.identities_root, self.identity_name, "/layouts/", object_name + '.html.erb')
-
-        when :view
-          File.file? File.join(Identity.identities_root, self.identity_name, "/views/", object_name + '.html.erb')
-          #        when :stylesheet
-          #          File.file? File.join(RAILS_ROOT, "public/stylesheets/themes", self.identity_name, object_name + '.css')
-        end
-      end
+#      private
+#      def self.has?(type, object_name)
+#        case type
+#        when :layout
+#          File.file? File.join(Identity.identities_root, self.identity_name, "/layouts/", object_name + '.html.erb')
+#
+#        when :view
+#          File.file? File.join(Identity.identities_root, self.identity_name, "/views/", object_name + '.html.erb')
+#          #        when :stylesheet
+#          #          File.file? File.join(RAILS_ROOT, "public/stylesheets/themes", self.identity_name, object_name + '.css')
+#        end
+#      end
     end
   end
 end
