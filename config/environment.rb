e30af5408b5572e508206d0238c19261786cd238
Igor Alexandrov <igor.alexandrov@gmail.com>
2010-03-13 22:52:45 +0300

    README file updated

--- config/environment.rb
+++ config/environment.rb
@@ -9,12 +9,13 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   require 'rubygems'
   
-  config.gem "authlogic"
-  config.gem "cancan"
-  config.gem "searchlogic"
-  config.gem "configatron"
-  config.gem "rainbow"
+  config.gem 'authlogic'
+  config.gem 'cancan'
+  config.gem 'searchlogic'
+  config.gem 'configatron'
+  config.gem 'rainbow'
   config.gem 'gravtastic'
+  config.gem 'sanitize'
 
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
