a5848f6b8c32c515eadc298560a05141d6a5852a
Igor Alexandrov <igor.alexandrov@gmail.com>
2010-03-19 15:06:37 +0300

    More simplified views

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,5 @@
 module CommentsHelper
+  def render_comment_details(comment, options = {})
+    render :partial => "comments/#{comment.class.to_s.underscore}_details", :locals => {:comment => comment}
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -11,9 +11,11 @@ module PostsHelper
   end
   
   def post_category_link(post, html_options = {})
-    link_to post.category.title,
-            category_path(post.category),
-            {:class => "g-link-no-visited"}.merge(html_options) { |key, v1, v2| v1 + ' ' + v2 }
+    cache category_path(post.category_id, :action_suffix => "post_title") do
+      link_to post.category.title,
+              category_path(post.category),
+              {:class => "g-link-no-visited"}.merge(html_options) { |key, v1, v2| v1 + ' ' + v2 }
+    end
   end
   
   def edit_post_content_link(post, html_options = {})
@@ -24,12 +26,34 @@ module PostsHelper
     end
   end
   
+  def new_post_comment_link(post, name, title = nil, parent_comment_id = 0, parent_comment_depth = 0)
+    title = name if title.nil?
+    if can? :create, Comment.new(:depth => parent_comment_depth)
+      link_to_remote content_tag(:span, name, :class => "g-javascript_link__value"),
+                          {:url => { :controller => "comments", :action => "new", :post_id => post.id,
+                                      :parent_comment_id => parent_comment_id}, :method => "get", },
+                                      :href => new_post_comment_url(post.id, :anchor => "new_comment"),
+                                      :class => "g-javascript_link",
+                                      :title => title
+    end
+  end
+  
   def render_post_content(post)
     render :partial => "posts/#{post.content.class.to_s.downcase.pluralize}/show", :locals => {:content => post.content}
   end
   
   def render_post_preview(post)
     render :partial => "posts/preview", :locals => {:post => post}
+  end
+  
+  def render_post_details(post, options = {})
+    render :partial => "posts/post_details", :locals => { :post => post, :options => options }
+  end
+  
+  def render_post_comments(post, parent_comment_id = 0, html_options = {})
+    content_tag(:ul, :id => "comment_#{parent_comment_id}_children", :class => "b-post__comments_list") do
+      render :partial => "comments/show", :collection => @comments.select { |comment| comment.parent_comment_id == parent_comment_id }, :as => :comment
+    end
   end
   
 end
