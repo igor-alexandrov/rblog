1ffa2b9ac952dc2b8f39c7f43f4b0ee4e6b94d7c
Andrey Kouznetsov <smixok@gmail.com>
2010-04-25 23:02:57 +0400

    removed unnecessary includes

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  require 'rubygems'
   
   config.gem 'authlogic'
   config.gem 'cancan'
@@ -33,7 +32,7 @@ Rails::Initializer.run do |config|
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
   # Activate observers that should always be running
-#  config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
