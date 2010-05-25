c1063555fab1a1954dc884c711449cff0064d47d
Andrey Kouznetsov <smixok@gmail.com>
2010-05-25 19:08:57 +0400

    revert environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,17 +1,20 @@
-RBlog::Application.configure do
-  config.cache_classes = false
+# Settings specified here will take precedence over those in config/environment.rb
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
+# In the development environment your application's code is reloaded on
+# every request.  This slows down response time but is perfect for development
+# since you don't have to restart the webserver when you make code changes.
+config.cache_classes = false
 
-  # Show full error reports and disable caching
-  config.action_controller.consider_all_requests_local = true
-  config.action_view.debug_rjs                         = true
-  config.action_controller.perform_caching             = false
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
 
-  # Don't care if the mailer can't send
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.raise_delivery_errors = true
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
 
-  config.cache_store = :file_store, 'tmp/cache' 
-end
+# Don't care if the mailer can't send
+config.action_mailer.delivery_method = :sendmail
+config.action_mailer.raise_delivery_errors = true
+
+config.cache_store = :file_store, 'tmp/cache' 
